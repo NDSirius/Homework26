d582be42746df2c8900a158eddc68fb25c8d097a
Sergey <sergey.u.severin@gmail.com>
2019-07-08 21:35:32 +0300

    text in tags

--- hw26-3.php
+++ hw26-3.php
@@ -1,6 +1,6 @@
 <?php
 $text = "<TITLE>Find the text enclosed in a tag from the HTML file and output it</TITLE>";
-preg_match("|<TITLE>(.*)</TITLE>|is", $text, $result);
+preg_match('|<TITLE>(.*)</TITLE>|is', $text, $result);
 $result_string = implode($result);
 var_dump($result_string);
 ?>
